43983abe3c0346377e3ec25d1a1f8b6fa142d8ed
abdalla_eltayeb <abdalla.eltayeb@campus.tu-berlin.de>
2022-03-31 20:32:19 +0000

    Upload New File

---
+++ Gyro.ino
@@ -0,0 +1,23 @@
+#include<Wire.h>
+
+const int MPU_addr = 0x68; // I2C address of the MPU-6050
+int16_t  GyZ;
+
+void SetupGyro()
+{
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+}
+
+float GyroAngle()
+{
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 14, true); // request a total of 14 registers
+  GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  return GyZ;
+}
